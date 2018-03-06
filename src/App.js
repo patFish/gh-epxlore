21f5c44e2e8c2ed5203b6191683d4148b22563cf
patFish <p.fischer@accenture.com>
2018-03-06 22:10:47 +0100

    added cardstack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { CardStack, Card } from "react-cardstack";
 
 class App extends Component {
   render() {
@@ -8,11 +9,28 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="App-title"> Welcome to React </h1>{" "}
+        </header>{" "}
+        <CardStack
+          height={1000}
+          width={1000}
+          background="#f8f8f8"
+          hoverOffset={25}
+        >
+          <Card background="#2980B9">
+            <h1>Number 1</h1>
+          </Card>
+
+          <Card background="#27AE60">
+            <h1>Number 2</h1>
+          </Card>
+          <Card background="#27CB60">
+            <h1>Number 3</h1>
+          </Card>
+          <Card background="#CBAE60">
+            <h1>Number 4</h1>
+          </Card>
+        </CardStack>
       </div>
     );
   }
